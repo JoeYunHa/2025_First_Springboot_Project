c036ab5606e4bad343f7489bf0a70a09557e4e1c
JoeYunHa <12201806@inha.edu>
2025-04-01 23:01:22 +0900

    v0.3 save DTO to DB

---
+++ src/main/java/com/example/firstproject/repository/ArticleRepository.java
@@ -0,0 +1,9 @@
+package com.example.firstproject.repository;
+
+import com.example.firstproject.entity.Article;
+import org.springframework.data.repository.CrudRepository;
+
+public interface ArticleRepository extends CrudRepository<Article, Long> {
+    // <Class type of Entity , Data type of representation or key? value>
+
+}
