c1cd0ac1792865a44156dcaf41b4d69609cd12cb
JoeYunHa <12201806@inha.edu>
2025-04-30 16:24:02 +0900

    v0.5 refactoring with lombok

--- src/main/java/com/example/firstproject/dto/ArticleForm.java
+++ src/main/java/com/example/firstproject/dto/ArticleForm.java
@@ -1,23 +1,14 @@
 package com.example.firstproject.dto;
 
 import com.example.firstproject.entity.Article;
+import lombok.AllArgsConstructor;
+import lombok.ToString;
 
+@AllArgsConstructor
+@ToString
 public class ArticleForm {
     private String title;
     private String content;
-    // generate constructor
-    public ArticleForm(String title, String content){
-        this.title = title;
-        this.content = content;
-    }
-    // override toString method to check
-    @Override
-    public String toString() {
-        return "ArticleForm{" +
-                "title='" + title + '\'' +
-                ", content='" + content + '\'' +
-                '}';
-    }
     // convert dto to entity
     public Article toEntity(){
         return new Article(null, title, content);
